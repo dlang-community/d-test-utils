206a2e6abd97b4462f3a320e4f2d23986fad3cff
Jan Jurzitza <gh@webfreak.org>
2020-06-08 11:32:22 +0200

    README + add recursive calls, VERSION=both & help

--- test_with_package.d
+++ test_with_package.d
@@ -1,60 +1,135 @@
 #!/usr/bin/env rdmd
 
-// Runs `dub test --compiler=$DC` with either the minimum available version on
-// DUB or the maximum available version on dub, respecting the dub.json version
-// range.
-//
-// Running with VERSION=min will use the lowest available version of a package,
-// for a version specification of format
-//   "~>0.13.0" this will run with "0.13.0",
-//   ">=0.13.0" this will run with "0.13.0",
-//   ">=0.13.0 <0.15.0" this will run with "0.13.0"
-//   otherwise error.
-//
-// Running with VERSION=max will use the highest available version of a package,
-// for a version specification of format
-//   "~>0.13.0" this will run with "~>0.13.0",
-//   ">=0.13.0" this will run with ">=0.13.0",
-//   ">=0.13.0 <0.15.0" this will run with "<0.15.0"
-//   otherwise error.
-//
-// By default this modifies the package "libdparse" but this can be modified by
-// specifying the $PACKAGE environment variable.
-//
-// Temporarily creates a dub.json file and renames the original to dub.1.json,
-// both of which is undone automatically on exit.
-//
-// dub upgrade will be run after creating the artificial dub.json and before
-// running the test command.
-//
-// If you run with `-- <command>` then that command will be run instead of
-// `dub test --compiler=$DC`
-//
-// The script returns 0 on success after all commands or 1 if anything fails.
+static immutable helpString = q"[Helper script test_with_package.d
+Usage:
+  VERSION=<min/max/both> PACKAGE=<name> rdmd test_with_package.d [-- command...]
+  VERSION=<min/max/both> rdmd test_with_package.d <package> [-- command...]
+  rdmd test_with_package.d <min/max/both> <package> [-- command...]
+
+Runs `dub test --compiler=$DC` with either the minimum available version on DUB
+or the maximum available version on dub, respecting the dub.json version range.
+
+Running with VERSION=min will use the lowest available version of a package,
+for a version specification of format
+  "~>0.13.0" this will run with "0.13.0",
+  ">=0.13.0" this will run with "0.13.0",
+  ">=0.13.0 <0.15.0" this will run with "0.13.0"
+  otherwise error.
+
+Running with VERSION=max will use the highest available version of a package,
+for a version specification of format
+  "~>0.13.0" this will run with "~>0.13.0",
+  ">=0.13.0" this will run with ">=0.13.0",
+  ">=0.13.0 <0.15.0" this will run with "<0.15.0"
+  otherwise error.
+
+Running with VERSION=both will run this script first with max, then with min.
+This is also the default mode when no VERSION is specified. For CI it may be
+better to explicitly state this for parallelism and cleaner test logs though.
+
+To specify a version, either set the VERSION environment variable or pass in the
+version (min/max/both) as first argument before the package name.
+
+Running this script either expects a package name as only or as second argument
+or if no arguments are given, through the PACKAGE environment variable.
+
+Temporarily creates a dub.json file and renames the original to dub.<n>.json,
+both of which is undone automatically on exit.
+
+`dub upgrade` will be run after creating the artificial dub.json and before
+running the test command. It might be necessary to call `dub upgrade` manually
+again after the test commands are finished to restore the dependencies back to
+the newest versions.
+
+If you run with `-- <command>` then that command will be run instead of
+`dub test --compiler=$DC`
+
+The script returns 0 on success after all commands or 1 if anything fails. If
+`both` is specified as version and both commands fail, this returns 2.
+]";
 
 import std;
 import fs = std.file;
 
 int main(string[] args)
 {
-	/// wanted target version (min or max)
-	const ver = environment.get("VERSION", "max");
+	/// wanted target version (min, max or both)
+	string ver = environment.get("VERSION", "both");
 	/// package to modify and test
-	const pkg = environment.get("PACKAGE", "libdparse");
+	string pkg = environment.get("PACKAGE", "");
 	/// D compiler to use
 	const dc = environment.get("DC", "dmd");
 
-	if (!ver.among!("min", "max"))
+	auto cmd = ["dub", "test", "--compiler=" ~ dc];
+	auto cmdIndex = args.countUntil("--");
+	if (cmdIndex != -1)
 	{
-		stderr.writefln("Unsupported version '%s', try min or max instead", ver);
+		cmd = args[cmdIndex + 1 .. $];
+		args = args[0 .. cmdIndex];
+	}
+
+	if (args.any!(among!("-h", "-help", "--help")))
+	{
+		stderr.writeln(helpString);
+		return 0;
+	}
+
+	if (args.length == 2)
+	{
+		// <program> <package>
+		pkg = args[1];
+	}
+	else if (args.length == 3)
+	{
+		// <program> <version> <package>
+		ver = args[1];
+		pkg = args[2];
+	}
+
+	if (!pkg.length)
+	{
+		stderr.writefln("No package specified. Try --help?");
 		return 1;
 	}
 
-	stderr.writeln("PACKAGE=", pkg);
-	stderr.writeln("VERSION=", ver);
-	stderr.writeln("DC=", dc);
+	if (ver == "both")
+	{
+		int result = 0;
+		result += doRun("max", pkg, dc, cmd);
+		stderr.writeln();
+		result += doRun("min", pkg, dc, cmd);
+		return result;
+	}
+	else
+	{
+		return doRun(ver, pkg, dc, cmd);
+	}
+}
+
+int doRun(string ver, string pkg, string dc, string[] cmd)
+{
+	if (!ver.among!("min", "max"))
+	{
+		stderr.writefln("Unsupported version '%s', try min, max or both instead",
+			ver);
+		return 1;
+	}
+
+	stderr.writefln("(PACKAGE=%s, VERSION=%s, DC=%s)", pkg, ver, dc);
+
+	if (!exists("dub.json"))
+	{
+		stderr.writefln("No dub.json file exists in the current working "
+			~ "directory '%s'! dub.sdl files are not supported.", getcwd());
+		return 1;
+	}
 
 	auto json = parseJSON(readText("dub.json"));
+	if ("dependencies" !in json || pkg !in json["dependencies"])
+	{
+		stderr.writefln("dub.json doesn't specify '%s' as dependency.", pkg);
+		return 1;
+	}
 	auto verSpec = json["dependencies"][pkg];
 	if (verSpec.type != JSONType.string)
 	{
@@ -70,10 +145,18 @@ int main(string[] args)
 
 	json["dependencies"][pkg] = JSONValue(determined);
 
-	// backup dub.json to dub.1.json and restore on exit
-	fs.rename("dub.json", "dub.1.json");
+	// backup dub.json to dub.<n>.json and restore on exit
+	string tmpDubName;
+	for (int n = 1;; n++)
+	{
+		// lots of GC alloc but it doesn't matter for a script like this
+		tmpDubName = "dub." ~ n.to!string ~ ".json";
+		if (!exists(tmpDubName))
+			break;
+	}
+	fs.rename("dub.json", tmpDubName);
 	scope (exit)
-		fs.rename("dub.1.json", "dub.json");
+		fs.rename(tmpDubName, "dub.json");
 
 	// create dummy dub.json and delete on exit
 	fs.write("dub.json", json.toPrettyString);
@@ -83,11 +166,6 @@ int main(string[] args)
 	stderr.writeln("$ dub upgrade");
 	if (spawnShell("dub upgrade").wait != 0)
 		return 1;
-
-	auto cmd = ["dub", "test", "--compiler=" ~ dc];
-	auto cmdIndex = args.countUntil("--");
-	if (cmdIndex != -1)
-		cmd = args[cmdIndex + 1 .. $];
 
 	stderr.writefln("$ %(%s %)", cmd);
 	if (spawnProcess(cmd).wait != 0)
@@ -139,6 +217,6 @@ string resolveVersion(string verRange, string wanted)
 		}
 	}
 	else
-		throw new Exception("Unsupported version range specifier to multi-test:"
+		throw new Exception("Unsupported version range specifier to multi-test: "
 			~ verRange);
 }
